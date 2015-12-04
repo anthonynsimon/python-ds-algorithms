c55f569ea3a1695232bc3e7abc80514eac2640f9
anthonynsimon <anthonynajjars@gmail.com>
2015-12-04 17:38:12 +0100

    Added Binary Search and Sequential Search examples

---
+++ SortAndSearch/BinarySearch.py
@@ -0,0 +1,58 @@
+class BinarySearch(object):
+    def __init__(self):
+        self.steps = 0
+
+    def containsValue(self, value, numbers):
+        found = self.containsValueRecursiveWorker(value, numbers)
+        result = ""
+
+        if found:
+            result = "List contains value. Took {0} steps.".format(self.steps)
+        else:
+            result = "List doesn't contain value. Took {0} steps.".format(self.steps)
+
+        self.steps = 0
+        return result
+
+
+    def containsValueWorker(self, value, numbers):
+        size = len(numbers)
+
+        if size == 0 or value > numbers[size-1]:
+            return False
+
+        while size > 1:
+            self.steps += 1
+            size = len(numbers)
+            middle = size//2
+
+            if value == numbers[middle]:
+                return True
+                break
+            elif value > numbers[middle]:
+                numbers = numbers[size//2:size]
+            elif value < numbers[middle]:
+                numbers = numbers[0:size//2]
+
+        return False
+
+    def containsValueRecursiveWorker(self, value, numbers):
+        self.steps += 1
+        size = len(numbers)
+
+        if size == 0 or value > numbers[size-1]:
+            return False
+
+        if value == numbers[size//2]:
+            return True
+        elif value < numbers[size//2]:
+            return self.containsValueRecursiveWorker(value, numbers[0:size//2])
+        elif value > numbers[size//2]:
+            return self.containsValueRecursiveWorker(value, numbers[size//2:size])
+
+numbers = []
+for i in range(10000000):
+    numbers.append(i)
+
+search = BinarySearch()
+print(search.containsValue(924681, numbers))
---
+++ SortAndSearch/SequencialSearch.py
@@ -0,0 +1,62 @@
+class SequentialSearch(object):
+    def containsValue(self, value, list):
+        size = len(list)
+
+        if size == 0:
+            return False
+
+        found = False
+
+        steps = 0
+        for i in range(size):
+            steps += 1
+
+            if list[i] == value:
+                found = True
+                break
+
+        result = ""
+        if found:
+            result = "List contains value. Took {0} steps.".format(steps)
+        else:
+            result = "List doesn't contain value. Took {0} steps.".format(steps)
+
+        return result
+
+    def containsValueOrderedList(self, value, list):
+        size = len(list)
+        found = False
+        result = ""
+
+        if size == 0:
+            return False
+
+        steps = 0
+        if value <= list[size-1]:
+            starting = 0
+
+            if value > list[size//2]:
+                starting = size//2
+
+            for i in range(starting, size):
+                steps += 1
+                if list[i] == value:
+                    found = True
+                    break
+
+        if found:
+            result = "List contains value. Took {0} steps.".format(steps)
+        else:
+            result = "List doesn't contain value. Took {0} steps.".format(steps)
+
+        return result
+
+
+numbers = []
+for i in range(10000000):
+    numbers.append(i)
+
+search = SequentialSearch()
+print(search.containsValue(924681,numbers))
+numbers.sort()
+print(search.containsValueOrderedList(924681,numbers))
