3c84468250828bdd8377c51d11b213902a45ff1f
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2015-12-08 18:36:35 +0100

    Wrote basic tests for sorting algorithms

---
+++ Tests/TestSortingAlgorithms.py
@@ -0,0 +1,44 @@
+import unittest
+from SortSearchAndHash import BubbleSort
+from SortSearchAndHash import SelectionSort
+from SortSearchAndHash import InsertionSort
+from SortSearchAndHash import ShellSort
+from SortSearchAndHash import MergeSort
+from SortSearchAndHash import QuickSort
+
+class TestSortingAlgorithms(unittest.TestCase):
+    def getUnsortedList(self):
+        return [14,17,15,6,4,0,19,7,13,12,20,10,2,5,1,11,18,16,3,9,8]
+
+    def getCorrectResult(self):
+        correctResult = []
+        for i in range(21):
+            correctResult.append(i)
+        return correctResult
+
+    def runTest(self, sortingAlgorithm):
+        numbers = self.getUnsortedList()
+        sortingAlgorithm.sort(numbers)
+        self.assertEqual(numbers, self.getCorrectResult())
+
+    def testBubbleSort(self):
+        self.runTest(BubbleSort.BubbleSort())
+
+    def testSelectionSort(self):
+        self.runTest(SelectionSort.SelectionSort())
+
+    def testInsertionSort(self):
+        self.runTest(InsertionSort.InsertionSort())
+
+    # def testShellSort(self):
+    #     self.runTest(ShellSort.ShellSort())
+    #
+    # def testMergeSort(self):
+    #     self.runTest(MergeSort.MergeSort())
+    #
+    # def testQuickSort(self):
+    #     self.runTest(QuickSort.QuickSort())
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSortingAlgorithms)
+unittest.TextTestRunner(verbosity=2).run(suite)
